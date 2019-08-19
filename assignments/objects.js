fa4127356bb40f4dbe415ad9d471769e9c531a34
Martins-O-U <martinsonyedikachi@gmail.com>
2019-08-19 13:45:37 +0100

    worked on the the objects created and tested it with a few functions that added keys and values to the objects

--- assignments/objects.js
+++ assignments/objects.js
@@ -28,27 +28,33 @@ const intern1 =
     gender: "Female"
   };
 
-// }
-//   id: 2,
-//   name: "Kennan",
-//   email: "kdben1@tinypic.com",
-//   gender: "Male"
+const intern2 = {
+  id: 2,
+  name: "Kennan",
+  email: "kdben1@tinypic.com",
+  gender: "Male"
+}
 
-//   id: 3,
-//   name:"Keven",
-//   email:"kmumery2@wikimedia.org",
-//   gender:"Male",
+const intern3 = {
+  id: 3,
+  name:"Keven",
+  email:"kmumery2@wikimedia.org",
+  gender:"Male",
+}
 
-//   id: 4,
-//   name:"Gannie",
-//   email:"gmartinson@illonois.edu",
-//   gender:"Male",
+const intern4 = {
+  id: 4,
+  name:"Gannie",
+  email:"gmartinson@illonois.edu",
+  gender:"Male",
+}
 
-//   id: 5,
-//   name:"Antonietta",
-//   email:"adaine5@samsung.com",
-//   gender:"Female",
-// }
+const intern5 = {
+  id: 5,
+  name:"Antonietta",
+  email:"adaine5@samsung.com",
+  gender:"Female",
+}
 
 
 // ==== Challenge 2: Reading Object Data ==== 
@@ -58,19 +64,31 @@ const intern1 =
 console.log(intern1.name);
 
 // Kennan's ID
+console.log(intern2.id);
 
 // Keven's email
+console.log(intern3.email);
 
 // Gannie's name
+console.log(intern4.name);
 
 // Antonietta's Gender
+console.log(intern5.gender);
 
 // ==== Challenge 3: Object Methods ==== 
 // Give Kennan the ability to say "Hello, my name is Kennan!" Use the console.log provided as a hint.
 // console.log(kennan.speak());
+// intern2.speak = `Hello, my name is ${intern2.name}`
+// console.log(intern2.speak);
+ //OR
+ intern2.speaks = ()=> `Hello, my named is ${intern2.name}`
+ console.log(intern2.speaks());
 
-// Antonietta loves math, give her the ability to multiply two numbers together and return the product. Use the console.log provided as a hint.
+// Antonietta loves math, give her the ability to multiply two numbers together and return the product. 
+//Use the console.log provided as a hint.
 //console.log(antonietta.multiplyNums(3,4));
+intern5.multiplyNums = (num1, num2)=> num1 * num2;
+console.log(intern5.multiplyNums(3,4));
 
 // === Great work! === Head over to the the arrays.js. You may come back and attempt the Stretch Challenge once you have completed the challenges in arrays.js and function-conversion.js.
 
